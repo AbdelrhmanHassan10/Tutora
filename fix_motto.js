133c2bfe0ab1442ad65f5b2eda85ee18333380de
Abdelrhman Hassan <Abdohsn20@gmail.com>
2026-06-02 22:31:25 +0300

    Clean up unused temporary translation and fix scripts

--- fix_motto.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let lang = fs.readFileSync('global-lang.js', 'utf8');
-
-lang = lang.replace(/"branding\.motto": "[^"]*"/g, '"branding.motto": "\\"حجر عتيق، روح رقمية\\""');
-
-fs.writeFileSync('global-lang.js', lang, 'utf8');
-console.log('global-lang.js updated');
--- inject_static.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('global-lang.js', 'utf8');
-const enInject = '"overview": "Overview", "details": "Details", "specifications": "Specifications", "backBtnText": "Back", "view_ar": "View in AR",';
-const arInject = '"overview": "نظرة عامة", "details": "التفاصيل", "specifications": "المواصفات", "backBtnText": "العودة", "view_ar": "عرض بالواقع المعزز",';
-content = content.replace('en: {', 'en: { ' + enInject);
-content = content.replace('ar: {', 'ar: { ' + arInject);
-fs.writeFileSync('global-lang.js', content, 'utf8');
-console.log('done');
